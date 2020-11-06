868074509992c8b5145b059c19783bbe98ddbf40
RdzCore <daniel_rod_con@hotmail.com>
2020-11-06 14:57:13 -0600

    add background styles an opacity

--- src/components/home.js
+++ src/components/home.js
@@ -1,8 +1,27 @@
 import React, { Component } from "react";
 
+import backgroudPicture from "../../static/assets/images/backgrounds/salchipapa.png";
+
 class Home extends Component {
   render() {
-    return <div className="home">Hola somos webUS</div>;
+    return (
+      <div className="home">
+        <div className="welcome-page">
+          <div
+            className="backgroud-img"
+            style={{
+              background: "url(" + backgroudPicture + ") no-repeat",
+              backgroundSize: "cover",
+              backgroundPosition: "center",
+            }}
+          />
+          <div className="backgroud-color" />
+          <div className="welcome-msg">
+            <h1>Hola somo webUs</h1>
+          </div>
+        </div>
+      </div>
+    );
   }
 }
 

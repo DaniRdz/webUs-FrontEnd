40d608b5a3406310587eb74f66d2840cb3caf96a
RdzCore <daniel_rod_con@hotmail.com>
2020-11-06 13:34:25 -0600

    add some styles to nav bar

--- src/components/navigation-container.js
+++ src/components/navigation-container.js
@@ -4,27 +4,36 @@ class NavigationContainer extends Component {
   render() {
     return (
       <div className="navigation-container">
-        <div className="logo">WebUs</div>
+        <div className="logo">
+          <div className="title">WebUs</div>
+          <div className="subtitle">con chalchicha</div>
+        </div>
         <div className="nav-links">
-          <a>Home</a>
-          <a>Menu</a>
-          <a>Acerca de Nosotros</a>
-          <a>Contacto</a>
+          <a className="link">Home</a>
+          <a className="link">Menu</a>
+          <a className="link">Acerca de Nosotros</a>
+          <a className="link">Contacto</a>
           {/* <a>Informacion de cuenta</a> */}
         </div>
         <div className="store-info">
           <div className="store-info-opennig">
-            <div className="openning-info"> Openning Hours</div>
-            <div className="hours">19:00-00:00</div>
+            <i className="far fa-clock icon"></i>
+            <div className="store-info-opennig-schedule">
+              <div className="openning-info"> Openning Hours</div>
+              <div className="hours">19:00-00:00</div>
+            </div>
           </div>
-          <div clasNAme="store-info-phone">
-            <div className="phone-info">(333)-333-33-33</div>
-            <div className="phone-title">webUs Phone</div>
+          <div className="store-info-phone">
+            <i className="fas fa-phone-volume icon"></i>
+            <div className="store-info-phone-number">
+              <div className="phone-info">(333)-333-33-33</div>
+              <div className="phone-title">webUs Phone</div>
+            </div>
           </div>
         </div>
         <div className="sigin-singup-links">
-          <a>Login</a>
-          <a>Register</a>
+          <a className="link">Login</a>
+          <a className="link">Register</a>
         </div>
       </div>
     );

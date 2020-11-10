8a14e3d4d38cec06d3aaa651db63f1410d7b5c94
RdzCore <daniel_rod_con@hotmail.com>
2020-11-10 11:21:26 -0600

    finish footer component styles

--- src/components/footer.js
+++ src/components/footer.js
@@ -6,10 +6,19 @@ export default function () {
   return (
     <div className="footer-wrapper">
       <Logo />
+      <div className="copyright-wrapper">
+        &copy; 2020 WEBUS con chalchica &#124; All right reserved.
+      </div>
       <div className="media-links-wrapper">
-        <i className="fab fa-facebook icon"></i>
-        <i className="fab fa-instagram icon"></i>
-        <i className="fab fa-twitter icon"></i>
+        <a href="#">
+          <i className="fab fa-facebook icon"></i>
+        </a>
+        <a href="#">
+          <i className="fab fa-instagram icon"></i>
+        </a>
+        <a href="#">
+          <i className="fab fa-twitter icon"></i>
+        </a>
       </div>
     </div>
   );

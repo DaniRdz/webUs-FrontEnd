338fc0a5eafa0730e87c25c0eb30259ca728ae61
RdzCore <daniel_rod_con@hotmail.com>
2020-11-10 14:14:16 -0600

    built signin component and styles css grid

---
+++ src/components/sign-in-form.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+export default class SignInForm extends Component {
+  render() {
+    return (
+      <div className="sign-in-form">
+        <h2>Sign in Form goes here</h2>
+      </div>
+    );
+  }
+}
--- src/components/sign-in.js
+++ src/components/sign-in.js
@@ -1,10 +1,23 @@
 import React, { Component } from "react";
 
+import SignInForm from "./sign-in-form";
+
+import wallpaper from "../../static/assets/images/wallpapers/salchipulpos-login.jpg";
+
 class SignIn extends Component {
   render() {
     return (
       <div className="sign-in-container">
-        <h1>Sing In</h1>
+        <div
+          className="welcome-img"
+          style={{
+            backgroundImage: "url(" + wallpaper + ")",
+            backgroundPosition: "center",
+            backgroundSize: "cover",
+          }}
+        />
+
+        <SignInForm />
       </div>
     );
   }

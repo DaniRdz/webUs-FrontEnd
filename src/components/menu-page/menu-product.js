4beead99ed04adc955ffb6b3e5e715ec4d234ca1
RdzCore <daniel_rod_con@hotmail.com>
2020-11-16 15:56:40 -0600

    add some styles to menu product component

--- src/components/menu-page/menu-product.js
+++ src/components/menu-page/menu-product.js
@@ -5,10 +5,14 @@ class MenuProduct extends Component {
     const { title, description, price, imgURL } = this.props;
     return (
       <div className="product">
-        {/* <img className="product-img" src={imgURL} /> */}
-        <div className="product-title">{title}</div>
-        <div className="product-description">{description}</div>
-        <div className="product-price">{price}</div>
+        <img className="product-img" src={imgURL} />
+        <div className="product-info">
+          <div className="product-info-title">{title}</div>
+          <div className="product-info-line"></div>
+          <div className="product-info-description">{description}</div>
+          <div className="product-info-price">{price}</div>
+          <button className="product-info-btn">Ordenar</button>
+        </div>
       </div>
     );
   }

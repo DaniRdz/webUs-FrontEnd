188191f61a289d8fd41d41e87883cdab6e81aa5a
RdzCore <daniel_rod_con@hotmail.com>
2020-11-23 13:30:21 -0600

    add some styles in review component

--- src/components/review-page/review.js
+++ src/components/review-page/review.js
@@ -5,16 +5,27 @@ export default class Review extends Component {
     return (
       <div className="review">
         <div className="review-title">Resumen de Orden</div>
-        <div className="review-products">products goes here</div>
+
+        <div className="review-info">
+          <div className="review-info-name">Nombre</div>
+          <div className="review-info-qty">Cantidad</div>
+          <div className="review-info-price">Precio</div>
+          <div className="review-info-line-top"></div>
+          <div className="review-info-products">products goes here</div>
+          <div className="review-info-line-bot"></div>
+          <div className="review-info-total">Total goes here</div>
+        </div>
+
         <div className="review-btns">
           <button
+            className="review-btns-back"
             onClick={() => {
               this.props.history.push("/menu");
             }}
           >
-            Regresar A Comprar
+            Regresar
           </button>
-          <button>Continuar con el pago</button>
+          <button className="review-btns-continue">Continuar</button>
         </div>
       </div>
     );

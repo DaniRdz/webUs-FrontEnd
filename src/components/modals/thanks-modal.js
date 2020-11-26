4107a41d753b854b281164925f7a62c8cdce57c3
RdzCore <daniel_rod_con@hotmail.com>
2020-11-26 14:38:26 -0600

    added line styles in thanks modal component

--- src/components/modals/thanks-modal.js
+++ src/components/modals/thanks-modal.js
@@ -1,10 +1,35 @@
 import React, { Component } from "react";
 import Modal from "react-modal";
 
+Modal.setAppElement(".app-wrapper");
+
 export default class ThanksModal extends Component {
+  constructor() {
+    super();
+    this.customStyles = {
+      content: {
+        top: "50%",
+        left: "50%",
+        right: "auto",
+        marginRight: "-50%",
+        transform: "translate(-50%, -50%)",
+        width: "1000px",
+        height: "450px",
+        backgroundColor: "#fffbf3",
+        border: "solid 3px #8c6100",
+      },
+      overlay: {
+        backgroundColor: "rgb(255, 251, 243, 0.75)",
+      },
+    };
+  }
   render() {
     return (
-      <Modal isOpen={this.props.showModal}>
+      <Modal
+        onRequestClose={() => this.props.handleCloseModal()}
+        isOpen={this.props.showModal}
+        style={this.customStyles}
+      >
         <a
           onClick={() => {
             this.props.handleCloseModal();
